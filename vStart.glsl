d0cccc2047b2ea7124aa26bb30cf874e6cd5662b
wimoMisterX <ran12us@gmail.com>
2016-05-17 19:10:11 +0800

    Part A done

--- vStart.glsl
+++ vStart.glsl
@@ -1,6 +1,8 @@
 attribute vec3 vPosition;
 attribute vec3 vNormal;
 attribute vec2 vTexCoord;
+attribute ivec4 boneIDs;
+attribute vec4 boneWeights;
 
 varying vec4 position;
 varying vec3 normal;
@@ -8,14 +10,19 @@ varying vec2 texCoord;
 
 uniform mat4 ModelView;
 uniform mat4 Projection;
+uniform mat4 boneTransforms[64];
 
 void main()
 {
-    vec4 vpos = vec4(vPosition, 1.0);
+    mat4 boneTransform = boneWeights[0] * boneTransforms[boneIDs[0]];
+    boneTransform += boneWeights[1] * boneTransforms[boneIDs[1]];
+    boneTransform += boneWeights[2] * boneTransforms[boneIDs[2]];
+    boneTransform += boneWeights[3] * boneTransforms[boneIDs[3]];
 
+    vec4 vpos = vec4(vPosition, 1.0) * boneTransform;
     gl_Position = Projection * ModelView * vpos;
 
     position = vpos;
-    normal = vNormal;
+    normal = mat3(boneTransform) * vNormal;
     texCoord = vTexCoord;
 }
